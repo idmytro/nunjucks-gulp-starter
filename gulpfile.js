6d87b46a73a10a596860e69f5e08bf62e8ed7bc0
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-01-20 01:48:19 +0200

    fix data watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,75 @@
-var gulp = require('gulp');
-var nunjucksRender = require('gulp-nunjucks-render');
-var data = require('gulp-data');
-var prettier = require('gulp-prettier');
+const gulp = require('gulp');
+const nunjucksRender = require('gulp-nunjucks-render');
+const data = require('gulp-data');
+const prettier = require('gulp-prettier');
+const fetch = require('node-fetch');
+const browserSync = require('browser-sync');
 
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
+const server = browserSync.create();
+const apiServer = browserSync.create();
+const API_PORT = 9999;
+const paths = {
+  pages: 'src/pages/**/*.+(html|njk|nunjucks)',
+  templates: ['src/templates'],
+  src: ['src/**/*.+(html|njk|nunjucks)', './data/data.json'],
+  dest: 'dist',
+  api: 'data',
+  data: './data/data.json',
+  dataServed: `http://localhost:${API_PORT}/data.json`,
+};
 
-gulp.task('nunjucks', () => {
-  return gulp.src('src/pages/**/*.+(html|njk|nunjucks)')
-    .pipe(data(() => require('./data/data.json')))
-    .pipe(nunjucksRender({ path: ['src/templates'] }))
+async function fetchData() {
+  // @ts-ignore
+  const response = await fetch(paths.dataServed)
+  const json = await response.json();
+  return json;
+}
+
+function build () {
+  return gulp.src(paths.pages)
+    .pipe(data(require(paths.data)))
+    .pipe(nunjucksRender({ path: paths.templates }))
     .pipe(prettier())
-    .pipe(gulp.dest('dist'))
-    .pipe(reload({stream: true}));
-});
+    .pipe(gulp.dest(paths.dest));
+}
 
-gulp.task('serve', gulp.series('nunjucks', () => {
-  browserSync.init({server: './dist'});
-  gulp.watch('src/**/*.+(html|njk|nunjucks)', gulp.parallel('nunjucks'));
-  gulp.watch('src/pages/**/*.html').on('change', reload);
-}));
+function rebuild () {
+  return gulp.src(paths.pages)
+    .pipe(data(fetchData))
+    .pipe(nunjucksRender({ path: paths.templates }))
+    .pipe(prettier())
+    .pipe(gulp.dest(paths.dest))
+    .pipe(server.stream());
+}
 
-gulp.task('default', gulp.series('serve'));
+function serve (done) {
+  server.init({
+    server: {
+      baseDir: paths.dest
+    }
+  });
+  done();
+}
+
+function api (done) {
+  apiServer.init({
+    server: {
+      baseDir: paths.api
+    },
+    port: API_PORT,
+    open: false
+  });
+  done();
+}
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+const watch = () => gulp.watch(paths.src, gulp.series(rebuild, reload));
+
+const dev = gulp.series(api, rebuild, serve, watch);
+
+exports.build = build;
+exports.dev = dev;
