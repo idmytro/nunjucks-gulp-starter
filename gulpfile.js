7467f25c2efb4b79963e05684d729a1b780f619d
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-01-19 22:04:11 +0200

    use data and for

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 gulp.task('nunjucks', () => {
   return gulp.src('src/pages/**/*.+(html|njk|nunjucks)')
+    .pipe(data(() => require('./data/data.json')))
     .pipe(nunjucksRender({ path: ['src/templates'] }))
     .pipe(gulp.dest('dist'))
     .pipe(reload({stream: true}));
