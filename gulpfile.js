1555365c7b26a518469a08dda655801359a82f53
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-01-19 21:27:06 +0200

    gulp-4

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
-// var data = require('gulp-data');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('serve', ['nunjucks'], () => {
-  browserSync.init({server: './dist'});
-  gulp.watch('src/**/*.+(html|njk|nunjucks)', ['nunjucks']);
-  gulp.watch('src/pages/**/*.html').on('change', reload);
-});
-
 gulp.task('nunjucks', () => {
   return gulp.src('src/pages/**/*.+(html|njk|nunjucks)')
-    // .pipe(data(() => require('./data/data.json')))
     .pipe(nunjucksRender({ path: ['src/templates'] }))
     .pipe(gulp.dest('dist'))
     .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['serve']);
+gulp.task('serve', gulp.series('nunjucks', () => {
+  browserSync.init({server: './dist'});
+  gulp.watch('src/**/*.+(html|njk|nunjucks)', gulp.parallel('nunjucks'));
+  gulp.watch('src/pages/**/*.html').on('change', reload);
+}));
+
+gulp.task('default', gulp.series('serve'));
